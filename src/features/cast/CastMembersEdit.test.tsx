7346059799d6d2fb06e2ec3bdf6802cd93ccaf13
engendromestre <engendro.mestre@gmail.com>
2024-09-19 10:58:10 -0300

    coverage CastMembersEdit

--- src/features/cast/CastMembersEdit.test.tsx
+++ src/features/cast/CastMembersEdit.test.tsx
@@ -1,5 +1,10 @@
 import { delay, http, HttpResponse } from "msw"
-import { fireEvent, renderWithProviders, screen, waitFor } from "../../utils/test-utils"
+import {
+  fireEvent,
+  renderWithProviders,
+  screen,
+  waitFor,
+} from "../../utils/test-utils"
 import { baseUrl } from "../api/apiSlice"
 import { CastMembersEdit } from "./CastMembersEdit"
 import { setupServer } from "msw/node"
@@ -27,32 +32,60 @@ describe("CastMembers Edit", () => {
   afterAll(() => server.close())
   beforeAll(() => server.listen({ onUnhandledRequest: "bypass" }))
   afterEach(() => server.resetHandlers())
-  
+
   it("should render correctly", () => {
     const { asFragment } = renderWithProviders(<CastMembersEdit />)
     expect(asFragment()).toMatchSnapshot()
   })
 
   it("should handle submit", async () => {
-    renderWithProviders(<CastMembersEdit />);
-    const name = screen.getByTestId("name");
+    renderWithProviders(<CastMembersEdit />)
+    const name = screen.getByTestId("name")
 
     await waitFor(() => {
-      expect(name).toHaveValue("Cast Member 1");
-    });
+      expect(name).toHaveValue("Cast Member 1")
+    })
 
     await waitFor(() => {
-      const submit = screen.getByText("Save");
-      expect(submit).toBeInTheDocument();
-    });
+      const submit = screen.getByText("Save")
+      expect(submit).toBeInTheDocument()
+    })
 
-    const submit = screen.getByText("Save");
-    fireEvent.change(name, { target: { value: "Test" } });
-    fireEvent.click(submit);
+    const submit = screen.getByText("Save")
+    fireEvent.change(name, { target: { value: "Test" } })
+    fireEvent.click(submit)
 
     await waitFor(() => {
-      const text = screen.getByText("Cast member update successfully!");
-      expect(text).toBeInTheDocument();
-    });
-  });
+      const text = screen.getByText("Cast member update successfully!")
+      expect(text).toBeInTheDocument()
+    })
+  })
+
+  it("should handle submit error", async () => {
+    server.use(
+      http.patch(`${baseUrl}/cast-members/1`, async () => {
+        return HttpResponse.error()
+      }),
+    )
+
+    renderWithProviders(<CastMembersEdit />)
+    const name = screen.getByTestId("name")
+    await waitFor(() => {
+      expect(name).toHaveValue("Cast Member 1")
+    })
+
+    await waitFor(() => {
+      const submit = screen.getByText("Save")
+      expect(submit).toBeInTheDocument()
+    })
+
+    const submit = screen.getByText("Save")
+    fireEvent.change(name, { target: { value: "test1" } })
+    fireEvent.click(submit)
+
+    await waitFor(() => {
+      const text = screen.getByText("Failed to update cast member!")
+      expect(text).toBeInTheDocument()
+    })
+  })
 })
